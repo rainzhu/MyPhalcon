b14f14d9ff90f201d4094c0983b66a54ed6b5937
zhuyu <1359231385@qq.com>
2016-07-10 20:38:57 +0800

    修改调试

--- app/forms/ContactForm.php
+++ app/forms/ContactForm.php
@@ -13,22 +13,22 @@ class ContactForm extends Form
     {
         // Name
         $name = new Text('name');
-        $name->setLabel('Your Full Name');
+        $name->setLabel('你的全名');
         $name->setFilters(array('striptags', 'string'));
         $name->addValidators(array(
             new PresenceOf(array(
-                'message' => 'Name is required'
+                'message' => '名字是必填的'
             ))
         ));
         $this->add($name);
 
         // Email
         $email = new Text('email');
-        $email->setLabel('E-Mail');
+        $email->setLabel('邮件');
         $email->setFilters('email');
         $email->addValidators(array(
             new PresenceOf(array(
-                'message' => 'E-mail is required'
+                'message' => '邮件是必填的'
             )),
             new Email(array(
                 'message' => 'E-mail is not valid'
@@ -37,11 +37,11 @@ class ContactForm extends Form
         $this->add($email);
 
         $comments = new TextArea('comments');
-        $comments->setLabel('Comments');
+        $comments->setLabel('建议');
         $comments->setFilters(array('striptags', 'string'));
         $comments->addValidators(array(
             new PresenceOf(array(
-                'message' => 'Comments is required'
+                'message' => '建议是必填的'
             ))
         ));
         $this->add($comments);
