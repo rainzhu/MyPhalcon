26f261dbe97ff04d96e46f2d5db2ff7a134578b8
zhuyu <1359231385@qq.com>
2016-07-11 14:01:36 +0800

    修改调试

--- app/controllers/testController.php
+++ app/controllers/testController.php
@@ -2,22 +2,29 @@
 
 class TestController extends ControllerBase
 {
+
     public function initialize()
     {
         $this->tag->setTitle('this is a test');
         parent::initialize();
     }
 
+    public function testAction()
+    {
+        if (!$this->request->isPost()) {
+
+            $this->flash->notice('This is a sample application of the Phalcon Framework.
+                Please don\'t provide us any personal information. Thanks');
+            $this->flash->error('123456');
+        }
+    }
 //    public function testAction()
 //    {
-//        if (!$this->request->isPost()) {
-//
-//            $this->flash->notice('This is a sample application of the Phalcon Framework.
-//                Please don\'t provide us any personal information. Thanks');
-//        }
+//     $this->forward('/test/test');
+//     //self::forward('/test/test');
 //    }
-    public function indexAction()
+    public function  helloAction()
     {
-
+        parent::forward('/test/test');
     }
 }
--- app/views/index/index.volt
+++ app/views/index/index.volt
@@ -9,15 +9,15 @@
 
 <div class="row">
     <div class="col-md-4">
-        <h2>Manage Invoices Online</h2>
-        <p>Create, track and export your invoices online. Automate recurring invoices and design your own invoice using our invoice template and brand it with your business logo. </p>
+        <h2>网络游戏</h2>
+        <p> </p>
     </div>
     <div class="col-md-4">
-        <h2>Dashboards And Reports</h2>
-        <p>Gain critical insights into how your business is doing. See what sells most, who are your top paying customers and the average time your customers take to pay.</p>
+        <h2>单机游戏</h2>
+        <p></p>
     </div>
     <div class="col-md-4">
-        <h2>Invite, Share And Collaborate</h2>
-        <p>Invite users and share your workload as invoice supports multiple users with different permissions. It helps your business to be more productive and efficient. </p>
+        <h2>手机游戏</h2>
+        <p> </p>
     </div>
 </div>
